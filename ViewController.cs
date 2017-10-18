6c8897a12b3f8c76a04abaf9158029e64e87c37b
gregorye1 <32143199+gregorye1@users.noreply.github.com>
2017-10-18 13:56:41 -0600

    bmi calculator

--- ViewController.cs
+++ ViewController.cs
@@ -15,12 +15,31 @@ namespace BMICalculator
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
+            newButton.TouchUpInside += NewButton_TouchUpInside;
+        }
+
+        void NewButton_TouchUpInside(object sender, EventArgs e)
+        {
+            float height = float.Parse(heightTextField.Text);
+            float weight = float.Parse(weightTextField.Text);
+
+            float bmi = (weight / (height * height));
+
+            bmiLabel.Text = bmi.ToString();
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            // Release any cached data, images, etc that aren't in use. 
+        }
+
+        public override void TouchesBegan(Foundation.NSSet touches, UIEvent evt)
+        {
+            base.TouchesBegan(touches, evt);
+
+            this.View.EndEditing(true);
         }
     }
 }
