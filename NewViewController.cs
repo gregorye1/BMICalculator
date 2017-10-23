56acabfeafc2687c48e105e86540eb66e8e9626a
gregorye1 <32143199+gregorye1@users.noreply.github.com>
2017-10-23 13:30:32 -0600

    change

--- NewViewController.cs
+++ NewViewController.cs
@@ -7,8 +7,36 @@ namespace BMICalculator
 {
     public partial class NewViewController : UIViewController
     {
-        public NewViewController (IntPtr handle) : base (handle)
+        public float bmi = 21.5f;
+
+        public NewViewController(IntPtr handle) : base(handle)
         {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            EvaluateBMI();
+        }
+
+        void EvaluateBMI();
+        {
+            string result = String.Empty;
+
+            if (bmi <= 16)
+                result = "very low";
+            else if (bmi< 18.5)
+                result = "low";
+            else if (bmi < 25)
+				result = "normal";
+            else if (bmi < 30)
+                result = "high";
+            else
+                result = "too high";
+
+            newDetailLabel.Text = result;
         }
     }
 }
--- ViewController.cs
+++ ViewController.cs
@@ -6,6 +6,7 @@ namespace BMICalculator
 {
     public partial class ViewController : UIViewController
     {
+        float bmi = 21.5f;
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -24,7 +25,7 @@ namespace BMICalculator
             float height = float.Parse(heightTextField.Text);
             float weight = float.Parse(weightTextField.Text);
 
-            float bmi = (weight / (height * height));
+            bmi = (weight / (height * height));
 
             bmiLabel.Text = bmi.ToString();
         }
@@ -40,6 +41,15 @@ namespace BMICalculator
             base.TouchesBegan(touches, evt);
 
             this.View.EndEditing(true);
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, Foundation.NSObject sender)
+        {
+            base.PrepareForSegue(segue, sender);
+
+            var newViewController = segue.DestinationViewController as NewViewController;
+
+            newViewController.bmi = this.bmi;
         }
     }
 }
